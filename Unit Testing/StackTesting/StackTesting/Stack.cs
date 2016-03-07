11893442907f67d33fe6a5611edb7c73eb5dfb14
acraig94 <acraig94@gmail.com>
2016-03-07 22:57:41 +1300

    Unit Testing

---
+++ Unit Testing/StackTesting/StackTesting/Stack.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTesting
+{
+    public class Stack
+    {
+
+        public class StringNode
+        {
+            public StringNode Next;
+            public string Str;
+
+            public StringNode(string str)
+            {
+                Str = str;
+                //Next = null;
+                
+            }
+        }
+
+        StringNode headPointer; // = null;
+        StringNode tailPointer; // = null;
+
+        public Stack()
+        {
+            headPointer = null;
+            tailPointer = null;
+        }
+
+        public void Push(string newString)
+        {
+            StringNode newNode = new StringNode(newString);
+            /*if (!(newString is string))
+            {
+                throw new ArgumentException("Argument passed in is invalid");
+            }*/
+            if (headPointer == null)
+            {
+                headPointer = newNode;
+                tailPointer = newNode;
+            }            
+            else
+            {
+                tailPointer.Next = newNode;
+                tailPointer = newNode;
+            }            
+        }
+
+        public string Pop()
+        {            
+            string lastItem = tailPointer.Str.ToString();
+            StringNode nodeWalker = headPointer;
+            //StringNode nodeToDelete = tailPointer;
+            
+            if (headPointer == null)
+            {
+                // if pop is called on empty stack
+                throw new NullReferenceException("Can't call Pop on an empty Stack");
+            }
+
+            if (nodeWalker != tailPointer)
+            {
+                while (nodeWalker.Next != tailPointer)
+                {
+                    nodeWalker = nodeWalker.Next;
+                }
+                tailPointer = nodeWalker;
+            }
+            else
+            {
+                headPointer = null;
+                tailPointer = null;
+            }
+
+            return lastItem;
+            
+        }
+
+        public string Peek()
+        {
+            if (headPointer == null)
+            {
+                throw new NullReferenceException("Can't call Peek on an empty Stack");
+            }
+ 
+            return tailPointer.Str.ToString();
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            StringNode nodeWalker = headPointer;
+
+            while (nodeWalker != null)
+            {
+                count++;
+                nodeWalker = nodeWalker.Next;
+            }
+
+            return count;
+        }
+
+        public bool IsEmpty()
+        {
+            bool empty = false;
+
+            if (headPointer == null)
+            {
+                empty = true;
+            }
+
+            return empty;
+        }
+
+    }
+}
---
+++ Unit Testing/StackTesting/UnitTestForStackProject/UnitTest1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackTesting;
+
+namespace UnitTestForStackProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsEmpty_NewStack_ReturnsTrue()
+        {
+            Stack testStack = new Stack();
+
+            bool expected = true;
+
+            bool actual = testStack.IsEmpty();
+
+            Assert.AreEqual(expected, actual);            
+        }
+
+        [TestMethod]
+        public void IsEmpty_FullStack_ReturnsFalse()
+        {
+            Stack testStack = new Stack();
+
+            testStack.Push("Bob");
+            testStack.Push("Fred");
+
+            bool expected = false;
+
+            bool actual = testStack.IsEmpty();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_PushnItems_Returnsn()
+        {
+            Stack testStack = new Stack();
+
+            testStack.Push("Bob");
+            testStack.Push("Fred");
+
+            int expected = 2;
+
+            int actual = testStack.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Peek_PushItemx_Returnsx()
+        {
+            Stack testStack = new Stack();
+
+            testStack.Push("Bob");
+            testStack.Push("Fred");
+
+            string expected = "Fred";
+
+            string actual = testStack.Peek();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pop_Itemx_Returnsx()
+        {
+            Stack testStack = new Stack();
+
+            testStack.Push("Bob");
+            testStack.Push("Fred");
+
+            string expected = "Fred";
+
+            string actual = testStack.Pop();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Fails
+        [TestMethod]
+        public void Pop_DeleteTest_ReturnCountAfterPop()
+        {
+            Stack testStack = new Stack();
+            testStack.Push("Fred");
+            testStack.Pop();
+
+            int expected = 0;
+
+            int actual = testStack.Count();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        // Fails
+        [TestMethod]
+        public void IsEmpty_DeleteTest_ReturnTrue()
+        {
+            Stack testStack = new Stack();
+            testStack.Push("Fred");
+            testStack.Push("Max");
+            testStack.Push("Jim");
+            testStack.Push("Tom");
+            testStack.Pop();
+            testStack.Pop();
+            testStack.Pop();
+            testStack.Pop();
+
+            bool expected = true;
+
+            bool actual = testStack.IsEmpty();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Count_OnNewStack_Return0()
+        {
+            Stack testStack = new Stack();
+            
+            int expected = 0;
+
+            int actual = testStack.Count();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException),"Can't call Pop on an empty Stack")]
+        public void Pop_OnEmptyStack_ReturnException()
+        {
+            Stack testStack = new Stack();
+
+            testStack.Pop();
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException), "Can't call Peek on an empty Stack")]
+        public void Peek_nonString_ReturnException()
+        {
+            Stack testStack = new Stack();
+
+            testStack.Peek();
+
+        }
+
+        [TestMethod]
+        public void Peek_PushManyItems_ReturnLastItemPushed()
+        {
+            Stack testStack = new Stack();
+            testStack.Push("Fred");
+            testStack.Push("Max");
+            testStack.Push("Jim");
+            testStack.Push("Tom");
+            
+            string expected = "Tom";
+
+            string actual = testStack.Peek();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
