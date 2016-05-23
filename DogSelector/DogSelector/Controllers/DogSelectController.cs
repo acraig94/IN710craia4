8cd6c2f6b7eccfb3eb080c699d32bbc523ff5b11
acraig94 <acraig94@gmail.com>
2016-05-23 21:47:44 +1200

    MVC Dog Selector

---
+++ DogSelector/DogSelector/Controllers/DogSelectController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DogSelector.Models;
+
+namespace DogSelector.Controllers
+{   
+    public class DogSelectController : Controller
+    {
+        private List<Dog> db;
+        // GET: DogSelect
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SelectedDog(EScale activityLevel, EScale sheddingLevel, EScale groomingLevel, EScale intelligenceLevel, ELength coatlength, ESize size, bool goodWithChildren, bool drools)
+        {
+            DatabaseManager dbm = new DatabaseManager();
+            db = dbm.makeDatabase();
+
+            Dog nDog = new Dog();
+            nDog.ActivityLevel = activityLevel;
+            nDog.SheddingLevel = sheddingLevel;
+            nDog.GroomingLevel = groomingLevel;
+            nDog.IntelligenceLevel = intelligenceLevel;
+            nDog.Coatlength = coatlength;
+            nDog.Size = size;
+            nDog.GoodWithChildren = goodWithChildren;
+            nDog.Drools = drools;
+
+            nDog.BreedName = db[0].BreedName;
+            nDog.ImageName = db[0].ImageName;
+            nDog.DisplayName = db[0].DisplayName;
+
+            Dog recommendation = dbm.FindRecommodation(db, nDog);
+
+            return View(recommendation);
+        }
+
+
+
+    }
+}
---
+++ DogSelector/DogSelector/DatabaseManager.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DogSelector.Models;
+
+namespace DogSelector
+{
+    public class DatabaseManager
+    {
+        private const int NOPREFERENCE = 3;
+
+        private List<Dog> db { get; set; }
+
+        public DatabaseManager()
+        {
+            //db = makeDatabase();
+        }
+
+        public Dog FindRecommodation(List<Dog> d, Dog nDog)
+        {
+            Dog bestMatch = new Dog();
+
+            foreach (Dog i in d)
+            {
+                i.Points += IsAMatch((int)i.ActivityLevel, (int)nDog.ActivityLevel);
+                i.Points += IsAMatch((int)i.Coatlength, (int)nDog.Coatlength);
+                i.Points += IsAMatch((int)i.GroomingLevel, (int)nDog.GroomingLevel);
+                i.Points += IsAMatch((int)i.IntelligenceLevel, (int)nDog.IntelligenceLevel);
+                i.Points += IsAMatch((int)i.SheddingLevel, (int)nDog.SheddingLevel);
+                i.Points += IsAMatch((int)i.Size, (int)nDog.Size);
+                i.Points += IsAMatch(i.Drools, nDog.Drools);
+                i.Points += IsAMatch(i.GoodWithChildren, nDog.GoodWithChildren);
+            }
+
+            PrintInfo(d);
+
+            var best = from x in d
+                       orderby x.Points descending
+                       select x;
+            bestMatch = best.First();
+
+            return bestMatch;
+        }
+
+        public int IsAMatch(int a, int b)
+        {
+            if (a == b || b == NOPREFERENCE)
+            {
+                return 1;
+            }
+            else return 0;
+        }
+
+        public int IsAMatch(bool a, bool b)
+        {
+            if (a == b)
+            {
+                return 1;
+            }
+            else return 0;
+        }
+
+        public List<Dog> makeDatabase()
+        {
+            List<Dog> newDatabase = new List<Dog>();
+
+            Dog afghanHound = new Dog();
+            afghanHound.BreedName = "afghanHound";
+            afghanHound.DisplayName = "Afghan Hound";
+            afghanHound.ActivityLevel = EScale.High;
+            afghanHound.Coatlength = ELength.Long;
+            afghanHound.Drools = false;
+            afghanHound.GoodWithChildren = false;
+            afghanHound.GroomingLevel = EScale.High;
+            afghanHound.IntelligenceLevel = EScale.Low;
+            afghanHound.SheddingLevel = EScale.High;
+            afghanHound.Size = ESize.Large;
+            afghanHound.ImageName = "AfghanHound.jpg";
+            newDatabase.Add(afghanHound);
+
+
+            Dog bassetHound = new Dog();
+            bassetHound.BreedName = "BassetHound";
+            bassetHound.DisplayName = "Basset Hound";
+            bassetHound.ActivityLevel = EScale.Medium;
+            bassetHound.Coatlength = ELength.Short;
+            bassetHound.Drools = true;
+            bassetHound.GoodWithChildren = true;
+            bassetHound.GroomingLevel = EScale.Low;
+            bassetHound.IntelligenceLevel = EScale.Medium;
+            bassetHound.SheddingLevel = EScale.Low;
+            bassetHound.Size = ESize.Medium;
+            bassetHound.ImageName = "BassetHound.jpg";
+            newDatabase.Add(bassetHound);
+
+            Dog beagle = new Dog()
+            {
+                BreedName = "Beagle",
+                DisplayName = "Beagle",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Short,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.Medium,
+                IntelligenceLevel = EScale.Medium,
+                SheddingLevel = EScale.Low,
+                Size = ESize.Medium,
+                ImageName = "Beagle.jpg"
+            };
+            newDatabase.Add(beagle);
+
+            Dog bichonFrise = new Dog()
+            {
+                BreedName = "BichonFrise",
+                DisplayName = "Bichon Frise",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Medium,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.High,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.Low,
+                Size = ESize.Small,
+                ImageName = "Bichonfrise.jpg"
+            };
+            newDatabase.Add(bichonFrise);
+
+
+
+            Dog borzoi = new Dog()
+            {
+                BreedName = "Borzoi",
+                DisplayName = "Borzoi",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Long,
+                Drools = false,
+                GoodWithChildren = false,
+                GroomingLevel = EScale.High,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.High,
+                Size = ESize.Large,
+                ImageName = "Borzoi.jpg"
+            };
+            newDatabase.Add(borzoi);
+
+            Dog bulldog = new Dog()
+            {
+                BreedName = "Bulldog",
+                DisplayName = "Bull Dog",
+                ActivityLevel = EScale.Medium,
+                Coatlength = ELength.Short,
+                Drools = true,
+                GoodWithChildren = false,
+                GroomingLevel = EScale.Low,
+                IntelligenceLevel = EScale.Medium,
+                SheddingLevel = EScale.Low,
+                Size = ESize.Medium,
+                ImageName = "Bulldog.jpg"
+            };
+            newDatabase.Add(bulldog);
+
+
+            Dog cav = new Dog()
+            {
+                BreedName = "CavalierKingCharlesSpaniel",
+                DisplayName = "Cavalier King Charles Spaniel",
+                ActivityLevel = EScale.Medium,
+                Coatlength = ELength.Medium,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.High,
+                IntelligenceLevel = EScale.Medium,
+                SheddingLevel = EScale.Medium,
+                Size = ESize.Small,
+                ImageName = "CavalierKingCharlesSpaniel.jpg"
+            };
+            newDatabase.Add(cav);
+
+
+            Dog chowchow = new Dog()
+            {
+                BreedName = "Chowchow",
+                DisplayName = "Chow Chow",
+                ActivityLevel = EScale.Medium,
+                Coatlength = ELength.Long,
+                Drools = true,
+                GoodWithChildren = false,
+                GroomingLevel = EScale.High,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.High,
+                Size = ESize.Large,
+                ImageName = "Chowchow.jpg"
+            };
+            newDatabase.Add(chowchow);
+
+            Dog dalmation = new Dog()
+            {
+                BreedName = "Dalmation",
+                DisplayName = "Dalmation",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Short,
+                Drools = false,
+                GoodWithChildren = false,
+                GroomingLevel = EScale.Medium,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.Low,
+                Size = ESize.Large,
+                ImageName = "Dalmation.jpg"
+            };
+            newDatabase.Add(dalmation);
+
+            Dog goldenRetriever = new Dog()
+            {
+                BreedName = "GoldenRetriever",
+                DisplayName = "Golden Retriever",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Long,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.Medium,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.High,
+                Size = ESize.Large,
+                ImageName = "GoldenRetriever.jpg"
+            };
+            newDatabase.Add(goldenRetriever);
+
+            Dog maltese = new Dog()
+            {
+                BreedName = "Maltese",
+                DisplayName = "Maltese",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Long,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.High,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.High,
+                Size = ESize.Small,
+                ImageName = "Maltese.jpg"
+            };
+            newDatabase.Add(maltese);
+
+            Dog newfoundland = new Dog()
+            {
+                BreedName = "Newfoundland",
+                DisplayName = "Newfoundland",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Long,
+                Drools = true,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.Medium,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.High,
+                Size = ESize.Large,
+                ImageName = "newfoundland.jpg"
+            };
+            newDatabase.Add(newfoundland);
+
+            Dog oldEnglishSheepdog = new Dog()
+            {
+                BreedName = "OldEnglishSheepdog",
+                DisplayName = "Old English Sheepdog",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Long,
+                Drools = true,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.High,
+                IntelligenceLevel = EScale.Medium,
+                SheddingLevel = EScale.High,
+                Size = ESize.Large,
+                ImageName = "OldEnglishSheepdog.jpg"
+            };
+            newDatabase.Add(oldEnglishSheepdog);
+
+            Dog pug = new Dog()
+            {
+                BreedName = "Pug",
+                DisplayName = "Pug",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Short,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.Low,
+                IntelligenceLevel = EScale.Low,
+                SheddingLevel = EScale.Low,
+                Size = ESize.Small,
+                ImageName = "Pug.jpg"
+            };
+            newDatabase.Add(pug);
+
+
+            Dog westHighlandWhiteTerrier = new Dog()
+            {
+                BreedName = "WestHighlandWhiteTerrier",
+                DisplayName = "West Highland White Terrier",
+                ActivityLevel = EScale.High,
+                Coatlength = ELength.Medium,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = EScale.Medium,
+                IntelligenceLevel = EScale.High,
+                SheddingLevel = EScale.Medium,
+                Size = ESize.Small,
+                ImageName = "WestHighlandWhiteTerrier.jpg"
+            };
+            newDatabase.Add(westHighlandWhiteTerrier);
+
+            return newDatabase;
+        }
+
+        public void PrintInfo(List<Dog> d)
+        {
+            foreach (Dog i in d)
+                Console.WriteLine(i.BreedName + " = " + i.Points);
+        }
+
+
+    }
+}
